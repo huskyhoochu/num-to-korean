995ba7f305d329c08e657a22eca0ac0387f692aa
huskyhoochu <dfg1499@gmail.com>
2020-04-19 22:16:11 +0900

    Install webpack & minify

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.ts',
+  output: {
+    libraryTarget: 'umd',
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+}
