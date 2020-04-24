404e66d864df8ade4343481ebb0290c829154c68
huskyhoochu <dfg1499@gmail.com>
2020-04-24 17:55:10 +0900

    Install prettier & git hooks

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ export function splitEvery<T>(n: number, arr: Array<T>): Array<Array<T>> {
   let index = 0;
 
   while (index < arr.length) {
-    result.push(arr.slice(index, index += n));
+    result.push(arr.slice(index, (index += n)));
   }
 
   return result;
