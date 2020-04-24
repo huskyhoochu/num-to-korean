92d7e2ead20b7ba0e16e780b0b7f2299083706b3
huskyhoochu <dfg1499@gmail.com>
2020-04-24 17:46:58 +0900

    Linting files & setup eslint options

--- src/utils.ts
+++ src/utils.ts
@@ -12,9 +12,9 @@ export function splitEvery<T>(n: number, arr: Array<T>): Array<Array<T>> {
 export function flatten<T>(arr: Array<Array<T>>): Array<T> {
   const result = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      result.push(arr[i][j])
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr[i].length; j += 1) {
+      result.push(arr[i][j]);
     }
   }
 
